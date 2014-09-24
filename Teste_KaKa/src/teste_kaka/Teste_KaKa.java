c4f33df58e2059a86878cba7c4d5cfebabc672da
Elis <Elis@Elis-PC>
2014-09-24 12:55:18 -0300

    1o commit - Elis

---
+++ Teste_KaKa/src/teste_kaka/Teste_KaKa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package teste_kaka;
+
+/**
+ *
+ * @author Elis
+ */
+public class Teste_KaKa {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("1o commit - Elis");
+    }
+    
+}
